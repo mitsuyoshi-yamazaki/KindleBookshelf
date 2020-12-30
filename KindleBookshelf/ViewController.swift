f008bae49fa84c8afa565de910b398066ed8ee70
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2020-12-30 18:50:05 +0900

    プロジェクトを作成

---
+++ KindleBookshelf/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KindleBookshelf
+//
+//  Created by Yamazaki Mitsuyoshi on 2020/12/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
